b87a6410937a76ce8e9739404ddd7e17bc1a9302
Deddy Kakunsi <deddykakunsi@outlook.com>
2015-11-14 10:39:46 +0800

    Fix json type info.

--- test/com/dbsys/rs/hr/test/controller/Tests.java
+++ test/com/dbsys/rs/hr/test/controller/Tests.java
@@ -7,4 +7,6 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ ApotekerControllerTest.class, DokterControllerTest.class,
 		PekerjaControllerTest.class, PerawatControllerTest.class})
-public class Tests { }
+public class Tests {
+	
+}
